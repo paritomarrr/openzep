37a9851930371d71dfcabef4b9492f239d2b5f5b
Pari Tomar <68550407+paritomarrr@users.noreply.github.com>
2022-02-25 19:27:20 +0530

    ether

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,14 +1,20 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Box {
+// auth from access-control
+// import "./access-control/Auth.sol";
+
+// importing ownable contract from openzep contract lib
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
     uint256 private _value;
 
     //emit when the value gets change
     event valueChanged(uint256 value);
 
     // stores new value in the contract
-    function store(uint256 value) public {
+    function store(uint256 value) public onlyOwner {
         value = _value;
         emit valueChanged(value);
     }

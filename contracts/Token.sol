c3c6c3be3bdf1200ebc81c07768096fca7f5cc84
Pari Tomar <68550407+paritomarrr@users.noreply.github.com>
2022-02-27 14:10:07 +0530

    token contract

---
+++ contracts/Token.sol
@@ -0,0 +1,40 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Token {
+    // name variable to identify the token
+    string public name = "My Hardhat Token";
+    string public symbol = "MHT";
+
+    // fixed amt of token stored
+    uint256 public totalSupply = 1000000;
+
+    // var to store ethereum accs
+    address public owner;
+
+    // mapping used to store each acc balance
+    mapping(address => uint256) balances;
+
+    // contract initialization
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    // func to transfer tokens
+    // 'external' modifier makes func only callable from outside of contract
+    function transfer(address to, uint256 amount) external {
+        // check balance
+        require(balances[msg.sender] >= amount, "Not enough Tokens");
+
+        // transfer the amt
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    // read-only func to retrieve token balance of a given acc
+    // view -> view only, cannot modify the contract's state
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
